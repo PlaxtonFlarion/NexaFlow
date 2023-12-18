84a2181431cc78528250e8467433a51e02d2c156
AceKeppel <AceKeppel@outlook.com>
2023-12-18 20:38:11 +0800

    update

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='nexaflow',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/PlaxtonFlarion/NexaFlow',
+    license='MIT',
+    author='AceKeppel',
+    author_email='AceKeppel@outlook.com',
+    description='NexaFlow',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: JavaScript',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows :: Windows 11',
+        'Natural Language :: Chinese (Simplified)',
+        'Natural Language :: English',
+    ]
+)
