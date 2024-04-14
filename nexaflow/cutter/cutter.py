b989c4e4bdcf03c586018b2b1bb6ebf4986f769c
AceKeppel <AceKeppel@outlook.com>
2024-04-14 21:22:43 +0800

    bug fixes

--- nexaflow/cutter/cutter.py
+++ nexaflow/cutter/cutter.py
@@ -3,32 +3,30 @@ import time
 import typing
 import numpy as np
 from loguru import logger
-from typing import List, Tuple
 from concurrent.futures import ThreadPoolExecutor
 from nexaflow import toolbox
+from nexaflow.hook import BaseHook
+from nexaflow.video import VideoObject, VideoFrame
 from nexaflow.cutter.cut_range import VideoCutRange
 from nexaflow.cutter.cut_result import VideoCutResult
-from nexaflow.video import VideoObject, VideoFrame
-from nexaflow.hook import BaseHook
 
 
 class Window(object):
 
     def __init__(self, video: "VideoObject", *args):
         self.video = video
-        assert len(args) == 7, "需要7个参数"
-        (self.step, self.block, self.window_size, self.window_coefficient,
-         self.start, self.video_length, self.frame_total) = args
+        self.step, self.block, self.window_size, self.window_coefficient, *_ = args
+        *_, self.start, self.video_length, self.frame_total = args
         self.end = self.start + self.window_size * self.step
 
-    def load_data(self) -> typing.List[VideoFrame]:
-        cur = self.start
+    def load_data(self) -> list["VideoFrame"]:
+        current = self.start
         result = []
         video_operator = self.video.get_operator()
-        while cur <= self.end:
-            frame = video_operator.get_frame_by_id(cur)
+        while current <= self.end:
+            frame = video_operator.get_frame_by_id(current)
             result.append(frame)
-            cur += self.step
+            current += self.step
 
         if len(result) < 2:
             last = video_operator.get_frame_by_id(self.end)
@@ -45,7 +43,7 @@ class Window(object):
             self.end = self.video_length
         return True
 
-    def float_merge(self, float_list: typing.List[float]) -> float:
+    def float_merge(self, float_list: list[float]) -> float:
         length = len(float_list)
         result = 0.0
         denominator = 0.0
@@ -63,14 +61,14 @@ class VideoCutter(object):
         self,
         step: int = None,
         compress_rate: float = None,
-        target_size: typing.Tuple[int, int] = None,
+        target_size: tuple[int, int] = None,
     ):
 
         self.step = step or 1
         self.compress_rate = compress_rate
         self.target_size = target_size
 
-        self._hook_list: typing.List[BaseHook] = list()
+        self._hook_list: list["BaseHook"] = list()
         # compress_hook = CompressHook(
         #     overwrite=True, compress_rate=compress_rate, target_size=target_size
         # )
@@ -78,27 +76,27 @@ class VideoCutter(object):
         # self.add_hook(compress_hook)
         # self.add_hook(grey_hook)
 
-    def add_hook(self, new_hook: BaseHook):
+    def add_hook(self, new_hook: "BaseHook"):
         self._hook_list.append(new_hook)
         # logger.debug(f"add hook: {new_hook.__class__.__name__}")
 
     @staticmethod
-    def pic_split(origin: np.ndarray, block: int) -> typing.List[np.ndarray]:
-        result: typing.List[np.ndarray] = list()
+    def pic_split(origin: np.ndarray, block: int) -> list[np.ndarray]:
+        result: list[np.ndarray] = list()
         for each_block in np.array_split(origin, block, axis=0):
             sub_block = np.array_split(each_block, block, axis=1)
             result += sub_block
         return result
 
-    def _apply_hook(self, frame: VideoFrame, *args, **kwargs) -> VideoFrame:
+    def _apply_hook(self, frame: "VideoFrame", *args, **kwargs) -> "VideoFrame":
         for each_hook in self._hook_list:
             frame = each_hook.do(frame, *args, **kwargs)
         return frame
 
     @staticmethod
     def compare_frame_list(
-        src: typing.List[np.ndarray], target: typing.List[np.ndarray]
-    ) -> typing.List[float]:
+        src: list[np.ndarray], target: list[np.ndarray]
+    ) -> list[float]:
 
         ssim = 1.0
         mse = 0.0
@@ -122,7 +120,7 @@ class VideoCutter(object):
         return [ssim, mse, psnr]
 
     @staticmethod
-    def split_into_parts(value: int, parts: int) -> List[Tuple[int, int, int]]:
+    def split_range(value: int, parts: int) -> list[tuple[int, int, int]]:
         division, remainder = value // parts, value % parts
         result, current_start = [], 1
 
@@ -140,10 +138,9 @@ class VideoCutter(object):
 
         return result
 
-    def handler_frames(self, window: Window) -> typing.List[VideoCutRange]:
-        range_list_part = []
+    def window_slice(self, window: "Window") -> list["VideoCutRange"]:
 
-        def technique():
+        def cutting():
             frame_list = window.load_data()
             frame_list = [self._apply_hook(each) for each in frame_list]
 
@@ -174,9 +171,10 @@ class VideoCutter(object):
                 )
             )
 
+        range_list_part = []
         pbar = toolbox.show_progress(window.frame_total, 174, "Cutter")
         while True:
-            technique()
+            cutting()
             pbar.update(1)
 
             continue_flag = window.shift()
@@ -186,52 +184,51 @@ class VideoCutter(object):
 
         return range_list_part
 
-    def _convert_video_into_range_list(
-        self, video: VideoObject, block: int, window_size: int, window_coefficient: int
-    ) -> typing.List[VideoCutRange]:
+    def magic_frame_range(
+        self, video: "VideoObject", block: int, window_size: int, window_coefficient: int
+    ) -> list[VideoCutRange]:
 
-        step = self.step
         video_length = video.frame_count
-        range_list: typing.List[VideoCutRange] = list()
         logger.info(f"总帧数: {video_length} 片段数: {video_length - 1} 分辨率: {video.frame_size}")
 
-        window_list: List["Window"] = []
-        for index, parts in enumerate(self.split_into_parts(video_length, 2)):
+        window_list = []
+        for index, parts in enumerate(self.split_range(video_length, 1 if video_length < 500 else 2)):
             start, end, size = parts
             logger.info(f"帧片段: {index + 1:02} Start: {start:03} End: {end:03} Length: {size:03}")
-            window = Window(video, step, block, window_size, window_coefficient, start, end, size)
+            window = Window(
+                video, self.step, block, window_size, window_coefficient, start, end, size
+            )
             window_list.append(window)
 
-        with ThreadPoolExecutor() as executor:
-            futures = [executor.submit(self.handler_frames, w) for w in window_list]
-            for future in futures:
-                range_list.extend(future.result())
+        with ThreadPoolExecutor() as exe:
+            futures = [
+                exe.submit(self.window_slice, window) for window in window_list
+            ]
+            range_list = [
+                part for future in futures for part in future.result()
+            ]
 
         return range_list
 
     def cut(
         self,
-        video: typing.Union[str, VideoObject],
-        block: int = None,
-        window_size: int = None,
-        window_coefficient: int = None,
-        *_,
-        **kwargs,
-    ) -> VideoCutResult:
+        video: typing.Union[str, "VideoObject"],
+        block: int = None, window_size: int = None, window_coefficient: int = None,
+        *_, **kwargs,
+    ) -> "VideoCutResult":
 
-        if not block:
-            block = 3
-        if not window_size:
-            window_size = 1
-        if not window_coefficient:
-            window_coefficient = 2
+        block = block or 3
+        window_size = window_size or 1
+        window_coefficient = window_coefficient or 2
 
         start_time = time.time()
-        if isinstance(video, str):
-            video = VideoObject(video)
+        video = VideoObject(video) if isinstance(video, str) else video
 
         logger.info(f"开始压缩视频: {os.path.basename(video.path)}")
-        range_list = self._convert_video_into_range_list(
+        # 如果视频包含 100 帧
+        # 从1开始，列表长度是99，而不是100
+        # [范围(1-2)、范围(2-3)、范围(3-4) ... 范围(99-100)]
+        range_list = self.magic_frame_range(
             video, block, window_size, window_coefficient
         )
         logger.info(f"视频压缩完成: {os.path.basename(video.path)}")
